0e535329f9a6264e33dc92b6679cbf7ded29cfcf
Nikolay Udovik <nikolay.udovik@outlook.com>
2021-09-23 15:08:57 +0300

    wip - banana

--- Lesson6_data_aggregation/Lesson6_data_aggregation.sql
+++ Lesson6_data_aggregation/Lesson6_data_aggregation.sql
@@ -1,42 +1,3 @@
-show tables;
-
-
-
-WITH friends AS (
-	SELECT
-		IF(user_id = @target_user, user_id, friend_id) AS user_id,
-		CASE @target_user
-			WHEN friend_id THEN user_id
-			ELSE friend_id
-		END AS friend_id
-	FROM
-		friendship
-	WHERE
-		request_type_id = 1
-		AND confirmed_at IS NOT NULL
-		AND (user_id = @target_user OR friend_id = @target_user)
-	UNION 
-	SELECT
-		user_id, friend_id
-	FROM
-		friendship
-	WHERE
-		request_type_id = 2
-		AND user_id = @target_user
-		AND friend_id IN (
-			SELECT
-				user_id
-			FROM
-				friendship
-			WHERE
-				request_type_id = 2
-				AND friend_id = @target_user
-	)
-)
-SELECT friend_id FROM friends WHERE user_id = @target_user; -- get friends
-
-select from_user_id, to_user_id from messages where is_delivered = 1 and (from_user_id = @target_user or to_user_id = @target_user) order by from_user_id ; -- get messages
-
 /*
 1. Проанализировать запросы, которые выполнялись на занятии, определить возможные корректировки и/или улучшения (JOIN пока не применять).
  */
@@ -103,9 +64,47 @@ GROUP BY friend_id
 ORDER BY total_messages DESC
 LIMIT 1;
 
+
+
+
 /*
 3. Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
 */
+
+
+select version();
+select * from profiles limit 5;
+select * from likes limit 5;
+desc likes;
+select * from target_types;
+
+SELECT user_id FROM profiles ORDER BY birthday ASC LIMIT 10 ; -- get top 10 the youngest users
+
+WITH youngest_10 AS (
+	SELECT 
+		user_id 
+	FROM profiles 
+	ORDER BY birthday ASC LIMIT 10
+)
+-- select * from youngest_10;
+SELECT 
+	COUNT(1) AS likes,
+	CASE 
+		WHEN target_id IS NULL THEN 'Total'
+		ELSE target_id
+	END AS user_id
+FROM 
+	likes
+WHERE 
+	target_id IN (SELECT user_id FROM youngest_10)
+	-- 	AND target_type_id = 1 -- no idea if i need to consider it...
+GROUP BY target_id
+WITH ROLLUP;
+
+
+
+
+
 
 /*
 4. Определить кто больше поставил лайков (всего) - мужчины или женщины?
