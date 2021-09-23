3107f0c20d737184b76c8f293d682292bee236e6
Nikolay Udovik <nikolay.udovik@outlook.com>
2021-09-23 12:54:53 +0300

    task2 done

--- Lesson6_data_aggregation/Lesson6_data_aggregation.sql
+++ Lesson6_data_aggregation/Lesson6_data_aggregation.sql
@@ -35,7 +35,7 @@ WITH friends AS (
 )
 SELECT friend_id FROM friends WHERE user_id = @target_user; -- get friends
 
-select * from messages where from_user_id = @target_user or to_user_id = @target_user; -- get messages
+select from_user_id, to_user_id from messages where is_delivered = 1 and (from_user_id = @target_user or to_user_id = @target_user) order by from_user_id ; -- get messages
 
 /*
 1. Проанализировать запросы, которые выполнялись на занятии, определить возможные корректировки и/или улучшения (JOIN пока не применять).
@@ -46,10 +46,9 @@ select * from messages where from_user_id = @target_user or to_user_id = @target
 Из всех друзей этого пользователя найдите человека, который больше всех общался с нашим пользователем.
 */
 
--- loking for dialogs and not one way messages
 
 -- set target user here
-SET @target_user := FLOOR(1 + 5 * RAND());
+SET @target_user := 3; -- FLOOR(1 + 5 * RAND());
 SELECT @target_user;
 
 
@@ -84,39 +83,25 @@ WITH friends AS (
 				AND friend_id = @target_user
 	)
 )
+-- TODO: Think about how to lookup for dialogs and not for one way messages with no response. 
 SELECT
-	to_user_id,
-	from_user_id 
  	CASE @target_user
  		WHEN from_user_id THEN to_user_id
  		ELSE from_user_id 
- 	END AS from_user_id
+ 	END AS friend_id,
+ 	COUNT(1) as total_messages
 FROM messages
 WHERE
 	(
 		to_user_id IN (SELECT friend_id FROM friends WHERE user_id = @target_user) AND from_user_id = @target_user
 		OR 
 		from_user_id IN (SELECT friend_id FROM friends WHERE user_id = @target_user) AND to_user_id = @target_user
-	)		
-;
-
-SELECT
-	(SELECT name FROM communities c WHERE cu.community_id)
-FROM
-	communities_users cu
-WHERE cu.user_id IN (
-	SELECT friend_id FROM friends
-);
-SELECT 
-	from_user_id, 
-	to_user_id, 
-	COUNT(1) AS total
-FROM messages 
-WHERE from_user_id = @target_user
-	OR to_user_id = @target_user
-GROUP BY from_user_id, to_user_id
-ORDER BY total DESC;
-
+	)
+	AND
+		is_delivered = 1
+GROUP BY friend_id
+ORDER BY total_messages DESC
+LIMIT 1;
 
 /*
 3. Подсчитать общее количество лайков, которые получили 10 самых молодых пользователей.
