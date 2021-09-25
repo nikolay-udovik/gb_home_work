be32a70e0b8c1b4e18af6e11a72af8b3d915b397
Nikolay Udovik <nikolay.udovik@outlook.com>
2021-09-25 15:22:05 +0300

    init

--- Lesson7_complex_queries/Lesson7_complex_queries.sql
+++ Lesson7_complex_queries/Lesson7_complex_queries.sql
@@ -2,15 +2,20 @@
 1. Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
 */
 
-
+show tables;
+select * from users;
+select * from orders;
 
 /*
 2. Выведите список товаров products и разделов catalogs, который соответствует товару.
 */
 
+select * from catalogs;
+select * from products;
 
 /*
 3. (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
 Поля from, to и label содержат английские названия городов, поле name — русское. 
 Выведите список рейсов flights с русскими названиями городов.
 */
+
---
+++ Lesson7_complex_queries/fill_up_tables.sql
@@ -0,0 +1,93 @@
+-- 40 users
+TRUNCATE users;
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('1', 'alias', '1977-10-08', '1989-01-19 20:29:32', '2013-10-13 20:25:36');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('2', 'ut', '1993-12-07', '1980-11-19 12:16:18', '2020-11-12 11:32:35');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('3', 'occaecati', '1983-01-24', '1976-04-27 08:08:38', '2019-10-21 09:51:27');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('4', 'quo', '2010-05-02', '1997-09-19 13:42:58', '1996-05-10 01:47:04');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('5', 'autem', '2007-07-23', '1998-10-30 11:40:07', '2001-10-08 09:15:48');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('6', 'est', '1987-06-14', '1997-01-26 17:29:58', '2017-06-25 07:14:30');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('7', 'vitae', '2018-11-06', '2018-02-08 21:01:35', '1991-08-06 00:40:52');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('8', 'vitae', '1990-08-18', '1995-07-14 09:33:29', '1989-12-04 08:48:34');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('9', 'et', '1970-08-07', '1984-05-11 00:03:48', '1970-09-25 23:54:17');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('10', 'tempora', '1974-10-15', '1992-02-13 22:45:22', '1990-02-21 19:11:35');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('11', 'enim', '1984-11-26', '2013-12-11 10:05:13', '2020-10-29 03:26:59');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('12', 'fugit', '2016-04-01', '1984-07-23 08:35:57', '1986-01-15 16:47:56');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('13', 'fuga', '1992-01-06', '1973-07-04 20:15:02', '1985-05-04 18:28:27');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('14', 'sit', '2005-11-15', '2008-05-03 08:08:25', '1993-09-25 13:48:38');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('15', 'et', '1970-07-08', '1989-08-10 06:50:07', '2003-11-12 03:58:48');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('16', 'quidem', '2020-09-07', '1994-12-13 11:23:06', '1973-02-11 20:38:00');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('17', 'laborum', '2010-01-08', '2005-06-18 09:33:21', '1995-01-23 12:19:26');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('18', 'fugiat', '1978-04-09', '2020-06-18 03:17:41', '2012-03-20 04:53:14');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('19', 'fuga', '2007-06-10', '2007-08-25 10:37:31', '2017-11-16 10:17:43');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('20', 'maiores', '2011-09-19', '2001-07-10 20:23:16', '2016-05-06 17:36:13');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('21', 'vitae', '2003-04-06', '2018-12-23 09:55:58', '2008-12-22 21:02:53');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('22', 'dolor', '2019-12-22', '2003-02-15 01:02:10', '2012-01-04 08:43:15');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('23', 'aut', '2014-01-19', '1996-11-10 01:11:31', '2011-10-05 01:00:16');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('24', 'ab', '1982-05-12', '2002-03-05 13:18:49', '2001-04-19 05:51:34');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('25', 'numquam', '1977-03-05', '1992-03-27 00:07:18', '1985-03-19 00:32:36');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('26', 'minus', '2019-05-22', '1993-10-24 04:53:58', '1985-05-14 11:52:56');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('27', 'debitis', '1996-09-08', '1996-08-04 14:23:44', '2008-11-21 02:57:27');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('28', 'ut', '1983-03-08', '2007-04-03 23:43:37', '1970-10-10 07:20:34');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('29', 'rerum', '1995-10-13', '2016-06-19 04:16:07', '1984-03-15 21:04:22');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('30', 'qui', '2011-07-07', '2008-06-27 04:47:48', '2003-10-29 03:23:11');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('31', 'nulla', '1978-05-07', '2012-06-24 04:07:03', '2019-02-24 01:15:10');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('32', 'veritatis', '2013-06-23', '1971-06-08 14:31:50', '2007-09-28 09:38:28');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('33', 'iste', '2006-05-06', '1989-09-06 09:54:43', '2005-06-21 08:31:12');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('34', 'quia', '1975-11-13', '1990-06-21 10:32:34', '1979-04-02 23:19:49');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('35', 'laudantium', '1975-06-26', '1985-08-12 11:43:26', '1997-05-08 12:30:56');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('36', 'quia', '1994-03-06', '2008-05-15 06:25:38', '2000-01-10 21:05:24');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('37', 'error', '1984-12-26', '1987-12-31 20:06:03', '2009-08-27 10:20:35');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('38', 'incidunt', '1990-04-24', '1996-01-17 15:50:42', '1987-06-09 09:58:23');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('39', 'doloremque', '1991-08-03', '1996-08-06 11:38:51', '1995-03-15 00:50:53');
+INSERT INTO `users` (`id`, `name`, `birthday_at`, `created_at`, `updated_at`) VALUES ('40', 'necessitatibus', '1985-03-23', '1976-06-03 12:35:45', '1973-11-14 21:59:44');
+
+-- 15 orders
+TRUNCATE orders;
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('1', 1, '2002-10-10 18:22:56', '2004-07-22 17:35:46');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('2', 2, '1989-09-01 11:14:01', '1981-04-12 22:22:02');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('3', 3, '2021-07-27 06:09:34', '1970-02-26 23:10:43');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('4', 4, '2006-01-24 18:24:56', '1998-11-15 04:45:09');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('5', 5, '1977-04-01 11:50:55', '1974-09-02 11:09:49');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('6', 6, '2008-01-06 01:52:05', '1992-02-26 02:13:27');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('7', 7, '2019-04-27 11:36:38', '1988-09-29 17:42:47');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('8', 8, '1986-06-06 05:23:56', '2016-05-12 21:19:48');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('9', 9, '1988-12-17 20:24:02', '2013-09-10 02:49:58');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('10', 10, '1984-07-05 14:50:35', '2000-08-17 10:27:27');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('11', 11, '2017-10-22 20:11:28', '1973-02-26 16:07:50');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('12', 12, '1988-02-17 13:51:31', '1992-04-19 13:41:44');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('13', 13, '1970-08-02 07:04:22', '2011-06-26 23:19:41');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('14', 14, '1972-09-21 03:31:13', '2009-11-23 20:53:04');
+INSERT INTO `orders` (`id`, `user_id`, `created_at`, `updated_at`) VALUES ('15', 15, '1993-10-04 16:29:37', '1974-09-05 16:49:26');
+
+
+
+
+-- for task 3
+DROP TABLE IF EXISTS flights;
+CREATE TABLE flights (
+	id SERIAL PRIMARY KEY,
+	`from` VARCHAR(100) NOT NULL,
+	`to` VARCHAR(100) NOT NULL
+);
+
+INSERT INTO flights (`from`, `to`)
+VALUES 
+	('moscow', 'omsk'),
+	('novgorod', 'kazan'),
+	('irkutsk', 'moscow'),
+	('omsk', 'irkutsk'),
+	('moscow', 'kazan');
+
+DROP TABLE IF EXISTS cities;
+CREATE TABLE cities (
+	label VARCHAR(100),
+	name VARCHAR(100)
+);
+
+INSERT INTO cities (label, name) 
+VALUES 
+	('moscow', 'Москва'),
+	('irkutsk', 'Иркутск'),
+	('novgorod', 'Новгород'),
+	('kazan', 'Казань'),
+	('omsk', 'Омск');
