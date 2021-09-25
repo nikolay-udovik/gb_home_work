af0a6794f1c2bb331e36f7bc988474ae1f97341f
Nikolay Udovik <nikolay.udovik@outlook.com>
2021-09-25 15:50:42 +0300

    task 1 and 2 done

--- Lesson7_complex_queries/Lesson7_complex_queries.sql
+++ Lesson7_complex_queries/Lesson7_complex_queries.sql
@@ -2,9 +2,17 @@
 1. Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
 */
 
-show tables;
-select * from users;
-select * from orders;
+-- INNER JOIN fits
+
+SELECT
+	u.name AS customer_name
+FROM 
+	users AS u
+JOIN 
+	orders AS o
+ON u.id = o.user_id 
+GROUP BY customer_name;
+
 
 /*
 2. Выведите список товаров products и разделов catalogs, который соответствует товару.
@@ -12,6 +20,16 @@ select * from orders;
 
 select * from catalogs;
 select * from products;
+
+SELECT 
+	p.name AS product, 
+	c.name AS catalog_name
+FROM 
+	products AS p 
+LEFT JOIN -- INNER should be ok too
+	catalogs AS c 
+ON c.id = p.catalog_id;
+
 
 /*
 3. (по желанию) Пусть имеется таблица рейсов flights (id, from, to) и таблица городов cities (label, name). 
