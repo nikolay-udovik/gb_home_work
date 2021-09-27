f742520ea6cf9140a760db0f99315f7b4ada2eb2
Nikolay Udovik <nikolay.udovik@outlook.com>
2021-09-27 19:01:26 +0300

    home work done

--- Lesson8_complex_queries/lesson8_comples_queries.sql
+++ Lesson8_complex_queries/lesson8_comples_queries.sql
@@ -1,10 +1,30 @@
--- my notes
--- \u l6
-
 /*
 1. Пусть задан некоторый пользователь. 
 Из всех пользователей соц. сети найдите человека, который больше всех общался с выбранным пользователем (написал ему сообщений).
 */
+
+SET @target_user := FLOOR(1 + 5 * RAND());
+SELECT @target_user;
+
+
+-- select from_user_id, count(1) as total from messages where is_delivered = 1 and to_user_id = @target_user group by from_user_id order by total desc;
+
+SELECT 
+	u.id,
+	CONCAT_WS(' ', u.first_name, u.last_name) AS name,
+	COUNT(1) AS total
+FROM messages m1
+-- left - if user has been deleted we still want to see his id. for example if our fk constraint do nothing on update.
+LEFT JOIN users u ON (
+	m1.from_user_id = u.id
+)
+WHERE
+	is_delivered = 1 
+	AND to_user_id = @target_user
+GROUP BY 
+	from_user_id 
+ORDER BY total DESC LIMIT 1;
+
 
 
 /*
@@ -19,9 +39,8 @@ SELECT
 		ELSE l.target_id 
 	END AS user_id,
 	COUNT(1) AS likes
-FROM 
-	profiles AS p 
-INNER JOIN likes AS l ON 
+FROM profiles p 
+INNER JOIN likes l ON 
 	(p.user_id = l.target_id)
 WHERE 
 	FLOOR(TIMESTAMPDIFF(YEAR, p.birthday, NOW())) < 10 
@@ -43,13 +62,12 @@ FROM
 	(
 		SELECT 
 			user_id 
-		FROM 
-			profiles 
+		FROM profiles 
 		WHERE 
 			FLOOR(TIMESTAMPDIFF(YEAR, birthday, NOW())) BETWEEN 0 AND 10
 	) AS y10
 INNER JOIN 
-	likes AS l ON (
+	likes l ON (
 		y10.user_id = l.target_id
 	)
 GROUP BY 
@@ -67,8 +85,8 @@ SELECT
 	u.gender,
 	COUNT(1)
 FROM 
-	likes AS l 
+	likes l 
 INNER JOIN
-	users AS u 
+	users u 
 ON (l.user_id = u.id)
 GROUP BY gender;
