5ab2dbb36dabb8c7ddb18a8395188527e045597f
Nikolay Udovik <nikolay.udovik@outlook.com>
2021-10-05 21:54:11 +0300

    3 done

--- Lesson9_transactions_vars_views_functions_triggers/P1_transactions_vars_views.sql
+++ Lesson9_transactions_vars_views_functions_triggers/P1_transactions_vars_views.sql
@@ -44,6 +44,47 @@ SELECT * FROM banana;
 если дата присутствует в исходном таблице и 0, если она отсутствует.
 */
 
+-- get ready.
+START TRANSACTION;
+
+DROP TABLE IF EXISTS august;
+CREATE TABLE august (
+	created_at DATE NOT NULL
+);
+
+DELIMITER //
+DROP PROCEDURE IF EXISTS insert_nums2august//
+CREATE PROCEDURE insert_nums2august()
+BEGIN
+	DECLARE num INT DEFAULT 1;
+	WHILE num <= 31 DO
+		INSERT INTO august (created_at) VALUES (CONCAT("2018-08-", num));
+		SET num = num + 1;
+	END WHILE;
+END//
+DELIMITER ;
+CALL insert_nums2august();
+
+-- 
+DROP TABLE IF EXISTS dates;
+CREATE TABLE dates (
+	created_at DATE NOT NULL
+);
+
+INSERT INTO dates (created_at) VALUES ('2018-08-01'), ('2016-08-04'), ('2018-08-16'), ('2018-08-17');
+
+COMMIT;
+
+-- let's start
+SELECT
+	august.created_at,
+	CASE 
+		WHEN dates.created_at IS NULL THEN '0'
+		ELSE '1'
+	END AS value
+FROM august 
+LEFT JOIN dates ON (
+august.created_at = dates.created_at); 
 
 
 
